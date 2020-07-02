7d387df3c3b621fc4f798787563a979c06bb34b2
Francisco <francisco59553@gmail.com>
2020-07-02 16:56:15 +0200

    feat: 1 endpoint added for testing

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,12 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { UsersModule } from './users/users.module';
-import { Connection } from 'typeorm';
 import { User } from './users/User';
 import { Account } from 'src/accounts/Account';
 import { Type } from 'src/types/Type';
 import { AccountsModule } from './accounts/accounts.module';
 import { TypesModule } from './types/types.module';
+import { AuthModule } from './auth/auth.module';
 
 
 @Module({
@@ -21,10 +21,8 @@ import { TypesModule } from './types/types.module';
     database: 'postgres',
     entities: [User, Account, Type],
     synchronize: false,
-  }), UsersModule, AccountsModule, TypesModule],
+  }), UsersModule, AccountsModule, TypesModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {
-  constructor(private connection: Connection){}
-}
+export class AppModule {}
--- api/src/users/users.controller.ts
+++ api/src/users/users.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { User } from './User';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(private readonly userService: UsersService) {}
+
+    @Get()
+    getAll(): Promise<User[]> {
+        return this.userService.findAll();
+    }
+}
